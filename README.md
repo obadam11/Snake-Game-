339d8882f1301552ef37ded9e6ac4f456e61f034
obadam11 <obada.mik.ab@gmail.com>
2020-05-14 01:25:09 +0300

    First Commit for the develop branch

---
+++ README.md
@@ -0,0 +1,6 @@
+*This is the Javascript Snake Game*<br/>
+
+*Controls*<br/>
+1. You can use WASD or Arrow keys to control the snake.
+2. When you lose you can right click to play again.
+3. Press Esc to toggle pausing.
--- snake.js
+++ snake.js
@@ -2,6 +2,8 @@ let cvs = document.getElementById("canvas");
 let ctx = cvs.getContext("2d");
 let coll = false;
 let PAUSE = false;
+let overScreen = false;
+let lose = false;
 const scale = 20;
 let ms = 75;
 const HEIGHT = cvs.height;
@@ -32,7 +34,6 @@ document.addEventListener("keydown", (e) => {
             dir = "L";
             snake.speedx = -snake.speed;
             snake.speedy = 0;
-            console.log("left");
         }
             
             
@@ -40,7 +41,6 @@ document.addEventListener("keydown", (e) => {
             dir = "U";
             snake.speedy = -snake.speed;
             snake.speedx = 0;
-            console.log("up");
         }
             
             
@@ -59,10 +59,25 @@ document.addEventListener("keydown", (e) => {
             pause();
         } 
 });
-
+document.addEventListener("mousedown", () => {
+    if (overScreen) playAgain();
+})
 let f = {x: snake.x, y: snake.y};
 snake.body.push(f);
 console.log(snake.body[0].x);
+
+function playAgain() {
+    // score = 0;
+    // for (let i = 0; i < snake.body.length; i++) {
+    //     snake.body.pop();
+    // }
+    // console.log(snake.body.length);
+    // snake.x = WIDTH / 2 - scale / 2;
+    // snake.y = HEIGHT / 2 - scale / 2;
+    // drawFood(food.x, food.y);
+    // lose = true;
+    location.reload();
+};
 
 function collision() {
     if (((snake.body[0].x == food.x) && (snake.body[0].y == food.y))) {
@@ -86,30 +101,40 @@ function pause() {
       PAUSE = true;
   }
 };
+function selfCollision() {
+    // The (i) is bugging when i <= 1.
+    for (let i = 0; i < snake.body.length; i++) {
+        if ((i >= 2) && (snake.body[0].x == snake.body[i].x) && (snake.body[0].y == snake.body[i].y)) {
+            console.log("Snake collided with itself", snake.body[i].x, snake.body[i].y, i);
+            return true;
+        }
+    }
+    return false;
+};
 function scoring() {
     ctx.font = "30px Comic Sans MS";
-    ctx.fillStyle = "#02FF3C";
+    ctx.fillStyle = "white";
     ctx.fillText(`Score: ${score}`, 20, 40);
 };
 function borders() {
-    if (snake.body[0].x <= 0) snake.body[0].x = 0;
-    if (snake.body[0].x >= WIDTH - snake.width) snake.body[0].x = WIDTH - snake.width;
-    if (snake.body[0].y <= 0) snake.body[0].y = 0;
-    if (snake.body[0].y >= HEIGHT - snake.width) snake.body[0].y =  HEIGHT - snake.width;
+    if (snake.body[0].x <= 0 - scale) return true;
+    else if (snake.body[0].x >= WIDTH - snake.width + scale) return true;
+    else if (snake.body[0].y <= 0 - scale) return true;
+    else if (snake.body[0].y >= HEIGHT - snake.width + scale) return true;
 };
 
 function drawSnake(x, y) {
     snake.body.forEach((value, i) => {
-        ctx.fillStyle = "#fff";
+        ctx.fillStyle = (i == 0) ? "#001BFF" : "#6081F0";
         ctx.fillRect(snake.body[i].x, snake.body[i].y, 20, 20);
     });
 };
 function drawFood(x, y) {
-    ctx.fillStyle = "red";
-    ctx.fillRect(x, y, food.width, food.height);
+     ctx.fillStyle = "red";
+     ctx.fillRect(x, y, food.width, food.height);
 };
 function drawBg() {
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "#1BFC00";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 };
 function update() {
@@ -123,31 +148,41 @@ function draw() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     drawBg();
     drawSnake(snake.x, snake.y);
-    if (!coll)
+    // Growing the snake part 🔥🔥🔥
+
+    // if the snake didn't eat the food
+    if (!coll) 
+    // Making the newHead object to save the coordinates of the snake's head
     {let newHead = {
         x : snake.x,
         y : snake.y
     };
 
-    snake.body.unshift(newHead);
-    snake.body.pop();
+    snake.body.unshift(newHead); // adding the object to the begging of the snake's body
+    snake.body.pop(); // and then removing the last part from the snak's body.
+    // Leaving us with a net parts of 0
 }
+// If the collision occured
 else {
+    // Making an object to save the coordinates of the snake's head
     let newHead = {
         x : snake.x,
         y : snake.y
     };
+    // adding the object to the begging of the snak's body
     snake.body.unshift(newHead);
+    // Reset coll to false to check it another collision with food occured.
     coll = false;
+    // Leaving us with a net parts of 1.
 }
-    // else {
-    //     snake.body.pop();
-    // }
     drawFood(food.x, food.y);
 };
 
 console.log(food.x, food.y);
-
+let newHead = {
+    x : snake.x,
+    y : snake.y
+};
 setInterval(function() {
     if (PAUSE) {
     ctx.fillStyle = "#fff";
@@ -157,7 +192,20 @@ setInterval(function() {
     ctx.fillText("Paused", WIDTH / 2 - 30, HEIGHT / 2);
     return;
     }
-    borders();
+    if ((selfCollision() || borders()) && !lose) {
+        // console.log("COLLSIION");
+        // ctx.fillStyle = "#fff";
+        // ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        // ctx.fillStyle = "blue";
+        // ctx.font = "30px Comic Sans MS";
+        // let text = (selfCollision()) ? "The Snake ate itself" : "The Snake fell off the screen";
+        // ctx.fillText(text, WIDTH / 2 - 200, HEIGHT / 2);
+
+        // ctx.font = "15px Comic Sans MS";
+        // ctx.fillText("Right click to play again", WIDTH / 2 - 100, HEIGHT / 2 + 150);
+        overScreen = true;
+        return;
+    }
     update();
     draw();
     collision();
